70dcae4684ed806e077b86e9724057ad67bd5919
Martin Marshall <law@martinmarshall.com>
2022-10-22 23:39:57 -0400

    Reduce mode-line spacing and add color to modified-indicator

--- mini-config/mini-packages.el
+++ mini-config/mini-packages.el
@@ -263,23 +263,27 @@ initially found within the current line."
 ;;; Bindings (built-in)
 
 ;; Mode-line customizations
+
 (mini-set
     mode-line-modified
   (list '(:eval
 	  (propertize
+	   ;; Set read-only-indicator as a lock symbol.
 	   (if buffer-read-only "🔒" "")
 	   'help-echo 'mode-line-read-only-help-echo
 	   'local-map (purecopy (make-mode-line-mouse-map
 				 'mouse-1
 				 #'mode-line-toggle-read-only))
 	   'mouse-face 'mode-line-highlight))
+	;; Set modified-indicator as a solid yellow circle.
 	'(:eval
 	  (propertize
 	   (if (buffer-modified-p) "🔴" "")
 	   'help-echo 'mode-line-modified-help-echo
 	   'local-map (purecopy (make-mode-line-mouse-map
 				 'mouse-1 #'mode-line-toggle-modified))
-	   'mouse-face 'mode-line-highlight))))
+	   'mouse-face 'mode-line-highlight
+	   'face '(:foreground "yellow")))))
 
 (mini-set
     mode-line-remote
@@ -299,6 +303,90 @@ initially found within the current line."
 						 default-directory)
 					      "Current directory is nil")))))))))
 
+;; Move `which-function' to end of mode-line.
+(mini-set
+    mode-line-misc-info
+  (list '(global-mode-string
+	  ("" global-mode-string))
+	'(which-function-mode
+	  (which-func-mode
+	   (" " which-func-format)))))
+
+;; Reduce spacing after column number.
+(mini-set
+    mode-line-position
+  (list '(:propertize
+	  ("" mode-line-percent-position)
+	  local-map #1=(keymap
+			(mode-line keymap
+				   (down-mouse-1 keymap
+						 (column-number-mode menu-item . #2=("Display Column Numbers" column-number-mode :help "Toggle displaying column numbers in the mode-line" :button
+										     (:toggle . column-number-mode)))
+						 (line-number-mode menu-item . #3=("Display Line Numbers" line-number-mode :help "Toggle displaying line numbers in the mode-line" :button
+										   (:toggle . line-number-mode)))
+						 (size-indication-mode menu-item . #4=("Display Size Indication" size-indication-mode :help "Toggle displaying a size indication in the mode-line" :button
+										       (:toggle . size-indication-mode)))
+						 #5="Toggle Line and Column Number Display")))
+	  display
+	  (min-width
+	   (5.0))
+	  mouse-face mode-line-highlight help-echo "Window Scroll Percentage
+mouse-1: Display Line and Column Mode Menu")
+	'(size-indication-mode
+	  (8
+	   #(" of %I" 0 6
+	     (local-map #1# mouse-face mode-line-highlight help-echo "Size indication mode
+mouse-1: Display Line and Column Mode Menu"))))
+	'(line-number-mode
+	  (column-number-mode
+	   (column-number-indicator-zero-based
+	    (10
+	     (:propertize mode-line-position-column-line-format
+			  ;; display
+			  ;; (min-width
+			  ;;  (10.0))
+			  . #6=(local-map
+				(keymap
+				 (mode-line keymap
+					    (down-mouse-1 keymap
+							  (column-number-mode menu-item . #2#)
+							  (line-number-mode menu-item . #3#)
+							  (size-indication-mode menu-item . #4#)
+							  #5#)))
+				mouse-face mode-line-highlight help-echo "Line number and Column number
+mouse-1: Display Line and Column Mode Menu")))
+	    (10
+	     (:propertize
+	      (:eval
+	       (string-replace "%c" "%C"
+			       (car mode-line-position-column-line-format)))
+	      ;; display
+	      ;; (min-width
+	      ;;  (10.0))
+	      . #6#)))
+	   (6
+	    (:propertize mode-line-position-line-format display
+			 (min-width
+			  (6.0))
+			 . #6#)))
+	  (column-number-mode
+	   (column-number-indicator-zero-based
+	    (6
+	     (:propertize mode-line-position-column-format display
+			  (min-width
+			   (6.0))
+			  . #6#))
+	    (6
+	     (:propertize
+	      (:eval
+	       (string-replace "%c" "%C"
+			       (car mode-line-position-column-format)))
+	      display
+	      (min-width
+	       (6.0))
+	      . #6#)))))))
+
+;; Improve look of `line-number-mode' and `column-number-mode'.
 (mini-set mode-line-position-column-line-format '("%l ǀ %c"))
 
 ;; This is not actually defined in bindings.el.  It's in C code, but
